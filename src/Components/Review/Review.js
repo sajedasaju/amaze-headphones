02d78b8fcdc9a54691b334b4875e54a1f8bcf5db
Sajeda Akhter <sajedasaju85@gmail.com>
2022-04-05 14:17:57 +0600

    ratings symbol added

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,4 +1,7 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Rating from 'react-rating';
 import './Review.css'
 
 const Review = ({ review }) => {
@@ -10,7 +13,13 @@ const Review = ({ review }) => {
             </div>
             <div className="review-text bg-white rounded-xl mt-3 mx-4 py-4">
                 <h2>Name: {name}</h2>
-                <span>Ratings: {rating}</span>
+                <span>Ratings: </span>
+                <Rating
+                    initialRating={rating}
+                    emptySymbol={<FontAwesomeIcon icon={faStar} style={{ color: 'rosybrown' }} />}
+                    fullSymbol={<FontAwesomeIcon style={{ color: 'indianred' }} icon={faStar} />}
+                    readonly
+                ></Rating>
             </div>
         </div>
     );
