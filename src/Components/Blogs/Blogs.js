c963df4b58b0572bf1fadc9efedc0c4460b7422e
Sajeda Akhter <sajedasaju85@gmail.com>
2022-04-05 14:21:06 +0600

    final

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Blogs.css'
 const Blogs = () => {
     return (
-        <div className='container mx-auto grid grid-rows-4'>
+        <div className='container mx-auto grid grid-rows-2'>
             <div className='bg-red-100 mt-8 rounded-2xl shadow-lg  '>
                 <h2 className='text-3xl font-bold text-rose-700  my-4 '>What is Context API?</h2>
                 <p className='text-xl text-left mb-8 px-8'>To avoid props drilling we use context api.It reduce duplication of code and increase  reusuability of code.Context API allows us to share state acress the entire app or part of it.THat means Context API give us a way to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.IT enables us to exchange unique details and assists in solving prop drilling from all levels of our application</p>
